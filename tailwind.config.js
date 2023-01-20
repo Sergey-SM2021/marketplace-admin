a5f05ad07fdbde2e02fee4ee0263726c6d171874
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-01-20 23:14:50 +0300

    INIT: установка зависимостей

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,sass,scss}"],
+  theme: {
+    colors: {
+      "white":"#fff",
+      "black": "#1A1A25",
+      "dark-gray": "#2A2C38",
+      "red": "#F53B49",
+      "purple": "#7c5ab8",
+    },
+  },
+  plugins: [],
+};
