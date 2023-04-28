a2ade2335a9a15e791e37229339ab37260eeee58
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-04-28 20:27:42 +0300

    del bredcrambs

--- src/modules/Product/components/Header.tsx
+++ src/modules/Product/components/Header.tsx
@@ -17,19 +17,6 @@ export const Header: FC<IHeader> = ({ productName }) => {
       <Button onClick={handlerBack}>back</Button>
       <div>
         <Subtitle>{productName}</Subtitle>
-        <div className="text-dark-gray font-bold">
-          <NavLink to={"/"} className="text-[#888] hover:text-[#333] font-bold">
-            электроника
-          </NavLink>
-          <span className="text-[#888]">/</span>
-          <NavLink to={"/"} className="text-[#888] hover:text-[#333] font-bold">
-            ноутбуки
-          </NavLink>
-          <span className="text-[#888]">/</span>
-          <NavLink to={"/"} className="text-[#888] hover:text-[#333] font-bold">
-            asermodel8291738_3782gryu...
-          </NavLink>
-        </div>
       </div>
     </div>
   )
