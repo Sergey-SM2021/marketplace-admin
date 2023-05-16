69a7e7fcc9376dc59e74ad6ea0e2260cf987ec5d
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-05-16 13:49:22 +0300

    setup vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import svgr from "vite-plugin-svgr"
+import viteTsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: [{ find:"@", replacement:"src" }],
-	},
+	plugins: [react(), viteTsconfigPaths(), svgr()],
 })
