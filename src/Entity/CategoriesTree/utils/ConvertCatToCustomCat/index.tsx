f05b3875f2f5846529f4d069ca4f9564f0a35c00
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-05-16 12:09:43 +0300

    fix: all eslint errors

--- src/Entity/CategoriesTree/utils/ConvertCatToCustomCat/index.tsx
+++ src/Entity/CategoriesTree/utils/ConvertCatToCustomCat/index.tsx
@@ -4,8 +4,6 @@ export const ConvertCatToCustomCat = (cat: Category): Category => {
 	return {
 		...cat,
 		childCategories:
-      cat.childCategories != null
-      	? cat.childCategories.map(chCat => ConvertCatToCustomCat(chCat))
-      	: [],
+      cat.childCategories != null ? cat.childCategories.map(chCat => ConvertCatToCustomCat(chCat)) : [],
 	}
 }
