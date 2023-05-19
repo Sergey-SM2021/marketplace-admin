481a58ccf7070152282a7dbc86fbab27ecf4a744
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-05-20 01:39:51 +0300

    refactoring

--- src/Entity/Products/model/products.tsx
+++ src/Entity/Products/model/products.tsx
@@ -1,10 +1,6 @@
 import * as api from "../api/api"
 
-import {
-	Product,
-	type GetProductsResponse,
-	type ProductResponseDTO,
-} from "Shared/types"
+import { type GetProductsResponse, type ProductResponseDTO } from "Shared/types"
 import { createDomain } from "effector"
 
 const productDomain = createDomain()
@@ -18,15 +14,14 @@ export const getProducts = productDomain.createEffect<
 >(api.getProducts)
 
 export const $products = productDomain
-	.createStore<Product[]>([])
-	.on(
-		getProducts.doneData,
-		(state, payload) => payload.products as ProductResponseDTO[]
-	)
+	.createStore<ProductResponseDTO[]>([])
+	.on(getProducts.doneData, (state, payload) => payload.products ?? [])
 	.on(removeProduct.done, (state, { params }) =>
 		state.filter(el => el.id !== params)
 	)
-	.on(updateProduct.doneData, (state, payload) =>
-		state.map(el => (el.id === payload?.id ? payload : el))
-	)
+	.on(updateProduct.doneData, (state, payload) => {
+		if (payload) {
+			return state.map(el => (el.id === payload?.id ? payload : el))
+		}
+	})
 	.on(createProduct.doneData, (state, payload) => [...state, payload.product])
--- src/widgets/Header/Header.data.tsx
+++ src/widgets/Header/Header.data.tsx
@@ -5,14 +5,20 @@ import { v4 } from "uuid"
 export const navLinks = [
 	{
 		icon: <Items className="w-5 h-5 fill-white" />,
+		text: "Orders",
+		id: v4(),
+		link: "/orders",
+	},
+	{
+		icon: <Items className="w-5 h-5 fill-white" />,
 		text: "Categories",
 		id: v4(),
-		link: "/admin/categories",
+		link: "/",
 	},
 	{
 		icon: <Products className="w-5 h-5 fill-white" />,
 		text: "Products",
 		id: v4(),
-		link: "/admin/products",
+		link: "/products",
 	},
 ]
