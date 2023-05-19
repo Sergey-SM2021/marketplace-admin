9180ea966d51c4f8460b523f2186d0aa07f23209
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-05-19 13:33:36 +0300

    refactoring

--- src/App/Providers/Router/model/router.tsx
+++ src/App/Providers/Router/model/router.tsx
@@ -2,13 +2,19 @@ import { CategoriesPage } from "pages/Categories"
 import { ProductPage } from "pages/ProductPage"
 import { ProductsPage } from "pages/Products"
 
-import { Layout } from "App/Providers/Layout"
-import { Navigate, createBrowserRouter } from "react-router-dom"
+import { Navigate, Outlet, createBrowserRouter } from "react-router-dom"
+import { Flex } from "@chakra-ui/layout"
+import { Header } from "widgets/Header"
 
 export const routes = createBrowserRouter([
 	{
 		path: "/",
-		element: <Layout />,
+		element: (
+			<Flex flexDirection={"column"} h={"100vh"} bg={"gray.300"} w={"full"}>
+				<Header></Header>
+				<Outlet />
+			</Flex>
+		),
 		children: [
 			{
 				path: "/",
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import { App } from "./App/App"
-import reportWebVitals from "./reportWebVitals"
 import ReactDOM from "react-dom/client"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(<App />)
-
-reportWebVitals()
